81b143e5d79673981a9749e046dfa32417f2ccb3
tursdlc <ana89.sdlc@gmail.com>
2024-08-05 22:50:50 +0200

    do part one

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,23 @@
+let number = 0;
 function add(){
-    //Escribir tu código que aumenta
+    let numberResult = document.getElementById("number");
+    numberResult.innerHTML = ++number;
 }
 
 function reset(){
     //Escribir tu código que hace un reset a 0
+    number = 0;
+    let numberResult = document.getElementById("number");
+    numberResult.innerHTML = number;
 }
 
 function decrease(){
     //Escribir tu código que resta
+    let numberResult = document.getElementById("number");
+    if(number > 0){
+        numberResult.innerHTML = --number;
+    }
+    
 };
 
 export {add, reset, decrease}
--- src/js/events.js
+++ src/js/events.js
@@ -1,4 +1,12 @@
 import {add, reset, decrease} from './counter.js'
 
+let addBtn = document.getElementById("add");
+addBtn.addEventListener("click", add);
+//Aquí estarán los eventos que ejecutarán
+//cada una de las funciones realizadas
 
-//Aquí estarán los eventos que ejecutarán cada una de las funciones realizadas
+let resetBtn = document.getElementById("reset");
+resetBtn.addEventListener("click", reset);
+
+let decreaseBtn = document.getElementById("decrease");
+decreaseBtn.addEventListener("click", decrease);
